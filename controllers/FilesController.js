4b81aa10533fdaeeef87fd4a62f6085e383ffe55
Yeeloman <yami.onlyme@gmail.com>
2024-05-15 01:53:53 +0100

    test

---
+++ controllers/FilesController.js
@@ -0,0 +1,56 @@
+import { UsersController } from "../controllers/UsersController";
+
+class FilesController {
+  static async postUpload(req, res) {
+    const user = await UsersController.getMe();
+    const userId = user.id;
+    const {
+      name, type, isPublic, data,
+    } = req.body;
+
+    if (!name) return res.status(400).json({ error: 'Missing name' });
+    if (!type || !['folder', 'file', 'image'].includes(type)) return res.status(400).json({ error: 'Missing type' });
+    if (!data && type !== 'folder') return res.status(400).json({ error: 'Missing data' });
+
+    let parentId = req.body.parentId || '0';
+    if (parentId !== '0') {
+      const parentFile = await dbClient.dbClient.collection('files').findOne({ _id: ObjectId(parentId) });
+      if (!parentFile) return res.status(400).json({ error: 'Parent not found' });
+      if (parentFile.type !== 'folder') return res.status(400).json({ error: 'Parent is not a folder' });
+    }
+    parentId = parentId !== '0' ? ObjectId(parentId) : '0';
+
+    const folderData = {
+      userId: ObjectId(userId),
+      name,
+      type,
+      isPublic: isPublic || false,
+      parentId,
+    };
+    if (type === 'folder') {
+      const newFolder = await dbClient.dbClient.collection('files').insertOne({
+        userId, name, type, isPublic: isPublic || false, parentId,
+      });
+      folderData.parentId = parentId === '0' ? 0 : ObjectId(parentId);
+      return res.status(201).json({ id: newFolder.insertedId, ...folderData });
+    }
+
+    const folderName = process.env.FOLDER_PATH || '/tmp/files_manager';
+    const fileId = uuidv4();
+    const localPath = path.join(folderName, fileId);
+
+    await fs.promises.mkdir(folderName, { recursive: true });
+    await fs.promises.writeFile(path.join(folderName, fileId), Buffer.from(data, 'base64'));
+
+    const newFile = await dbClient.dbClient.collection('files').insertOne({ localPath, ...folderData });
+
+    if (type === 'image') {
+      fileQueue.add({ fileId: newFile.insertedId, userId });
+    }
+
+    folderData.parentId = parentId === '0' ? 0 : ObjectId(parentId);
+    return res.status(201).json({ id: newFile.insertedId, localPath, ...folderData });
+  }
+}
+
+export default FilesController;
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,17 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/user/me', UsersController.getMe);
+
+router.post('/users', UsersController.postNew);
+router.post('.files', FilesController.postUpload);
 
 export default router;
